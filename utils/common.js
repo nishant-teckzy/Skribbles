dfcfd487f0a8f63ae79fc33f2ad6f89665d71b06
nishant-teckzy <nishanttiwari.dev@gmail.com>
2024-07-01 11:19:18 +0530

    code cleanup

--- utils/common.js
+++ utils/common.js
@@ -5,15 +5,9 @@ export function getRandomLine(callback){
       if(err) {
           throw err;
       }
-  
-      // note: this assumes `data` is a string - you may need
-      //       to coerce it - see the comments for an approach
       var lines = data.split('\n');
-      
-      // choose one of the lines...
       var line = lines[Math.floor(Math.random()*lines.length)]
-  
-      // invoke the callback with our line
+
       callback(line);
    })
   }
